b5e233aebd5eaf73436b61a317cfe1bb4b662173
astapchikartem <astapchikartem@outlook.com>
2024-10-26 14:15:00 +0000

    update invoice interface

--- contracts/interfaces/IInvoiceNFT.sol
+++ contracts/interfaces/IInvoiceNFT.sol
@@ -3,4 +3,17 @@ pragma solidity ^0.8.24;
 
 interface IInvoiceNFT {
     function markAsPaid(uint256 tokenId) external;
+    
+    function getInvoice(uint256 tokenId) external view returns (
+        uint256 id,
+        address creator,
+        address recipient,
+        uint256 amount,
+        address paymentToken,
+        string memory description,
+        uint256 dueDate,
+        uint8 status,
+        uint256 createdAt,
+        uint256 paidAt
+    );
 }
