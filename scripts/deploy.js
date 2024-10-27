a8e34bb07607e960a0f5fa673d13767e6acc83a3
astapchikartem <astapchikartem@outlook.com>
2024-10-27 16:45:00 +0000

    add deployment script skeleton

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+  console.log("Deploying contracts...");
+  // Deployment logic will be added
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
